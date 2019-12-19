d94ab7a5b5225db73600c0525ef1b5c1b904e7ad
lcx <leichunxiang58@163.com>
2019-12-19 11:58:09 +0800

    Initial Commit

---
+++ RunLoopDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RunLoopDemo
+//
+//  Created by lcx on 2019/12/19.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RunLoopDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RunLoopDemo
+//
+//  Created by lcx on 2019/12/19.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
